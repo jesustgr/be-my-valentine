6ae008d267a131b4d4e36bef6244b04e17e5529a
JT Reyes <jesustgreyes@gmail.com>
2024-02-07 21:19:52 -0800

    add alt to images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-"use client";
 import { useState } from "react";
 
 export default function App() {
@@ -37,12 +36,12 @@ export default function App() {
     <div className="flex flex-col items-center justify-center h-screen -mt-16">
       {yesPressed ? (
         <>
-        <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+        <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="" />
         <div className="text-4xl font-bold my-4">Ok yay!!!</div>
         </>
       ) : (
         <>
-          <img className="h-[200px]" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" />
+          <img className="h-[200px]" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" alt="" />
           <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
           <div>
             <button
